ce025aa35dd6e50c7f57589577a3af5c4e8b704f
ahmed el bayoumi <106277851+EngAhmedElBayoumi@users.noreply.github.com>
2022-08-11 04:28:47 +0200

    first commit

--- project/settings.py
+++ project/settings.py
@@ -81,8 +81,12 @@ WSGI_APPLICATION = 'project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'e_learning',
+        'USER':'postgres',
+        'PASSWORD':'admin@123',
+        'HOST':'localhost',
+        'PORT':'5433',
     }
 }
 
